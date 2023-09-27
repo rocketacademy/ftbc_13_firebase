d14981992a82be3d6ae8778f7723a006fee21ed8
FoongL <leung.foong@gmail.com>
2023-09-27 11:22:19 +0800

    added upload image feature

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 //import my firebase configged apps
-import { database } from "./firebase/firebase";
+import { database, storage } from "./firebase/firebase";
 
 // import firebase actions
 import {
@@ -15,6 +15,8 @@ import {
   onChildRemoved,
 } from "firebase/database";
 
+import { ref as sRef, uploadBytes, getDownloadURL } from "firebase/storage";
+
 const DB_STUDENTLIST_KEY = "studentList";
 class App extends React.Component {
   constructor(props) {
@@ -23,6 +25,7 @@ class App extends React.Component {
       firstName: "",
       description: "",
       studentList: [],
+      file: null,
     };
   }
 
@@ -60,6 +63,10 @@ class App extends React.Component {
     });
   }
 
+  testFunction = () => {
+    console.log("this is a test");
+  };
+
   onChange = (e) => {
     if (e.target.id === "firstName") {
       this.setState({
@@ -75,22 +82,42 @@ class App extends React.Component {
   // on submit, save a record into firebase
   handleSubmit = (e) => {
     e.preventDefault();
-    const dbRef = ref(database, DB_STUDENTLIST_KEY);
-    push(dbRef, {
-      name: this.state.firstName,
-      desc: this.state.description,
-      date: `${new Date()}`,
-    });
-
-    this.setState({
-      firstName: "",
-      description: "",
-    });
+    // push up image into storage
+    const fileRef = sRef(storage, `students/${this.state.file.name}`);
+    uploadBytes(fileRef, this.state.file)
+      .then(() => {
+        return getDownloadURL(fileRef);
+      })
+      .then((url) => {
+        console.log("url:", url);
+        console.log("or did this run first (INSIDE)");
+        // pushing up into realtime database
+        const dbRef = ref(database, DB_STUDENTLIST_KEY);
+        push(dbRef, {
+          name: this.state.firstName,
+          desc: this.state.description,
+          date: `${new Date()}`,
+          profilePic: url,
+        });
+      })
+      .then(() => {
+        this.setState({
+          firstName: "",
+          description: "",
+          file: null,
+        });
+      });
   };
 
   handleDelete = (e) => {
     const studentRef = ref(database, `${DB_STUDENTLIST_KEY}/${e.target.id}`);
     remove(studentRef);
+  };
+
+  fileChange = (e) => {
+    this.setState({
+      file: e.target.files[0],
+    });
   };
 
   render() {
@@ -115,6 +142,8 @@ class App extends React.Component {
               value={this.state.description}
             />
             <br />
+            <input type="file" onChange={(e) => this.fileChange(e)} />
+            <br />
             <input type="submit" />
           </form>
 
@@ -126,6 +155,8 @@ class App extends React.Component {
                 <br />
                 <p>{student.desc}</p>
                 <br />
+                <img src={student.profilePic} alt={student.name}></img>
+                <br/>
                 <button id={student.key} onClick={(e) => this.handleDelete(e)}>
                   delete me
                 </button>
